52d42033cb8b53e0dc71d79271f18d95e0115af2
Diego <dpampa20990@iesjoanramis.org>
2025-10-12 14:33:29 +0200

    Afegit càlcul de multiplicació

--- exemplo.java
+++ exemplo.java
@@ -2,5 +2,11 @@
 //segon comentari
 
 public class branques{
-
+	public static void main(String[] args){
+		int a = 5;
+		int b = 4;
+	
+		System.out.println("Producto de dos Números: "+(a*b));
+	
+	}
 }
